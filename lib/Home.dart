13ddbd300ece4f0172a673e24009c331b34c1dbe
William Muniz <william.muz@gmail.com>
2022-05-13 20:26:16 -0300

    first commit

---
+++ lib/Home.dart
@@ -0,0 +1,173 @@
+import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+import 'dart:io';
+import 'dart:async';
+import 'dart:convert';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  List _listaTarefas = [];
+  TextEditingController _controllerTarefa = TextEditingController();
+
+  Future<File> _getFile() async {
+    final diretorio = await getApplicationDocumentsDirectory();
+    return File("${diretorio.path}/dados.json");
+  }
+
+  _salvarTarefa() {
+    String textoDigitado = _controllerTarefa.text;
+
+    Map<String, dynamic> tarefa = Map();
+    tarefa["titulo"] = textoDigitado;
+    tarefa["realizada"] = false;
+
+    setState(() {
+      _listaTarefas.add(tarefa);
+    });
+    _salvarArquivo();
+
+    _controllerTarefa.text = "";
+  }
+
+  _salvarArquivo() async {
+    var arquivo = await _getFile();
+
+    String dados = json.encode(_listaTarefas);
+    arquivo.writeAsString(dados);
+  }
+
+  _lerArquivo() async {
+    try {
+      final arquivo = await _getFile();
+      return arquivo.readAsString();
+    } catch (e) {
+      return null;
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    _lerArquivo().then((dados) {
+      setState(() {
+        _listaTarefas = json.decode(dados);
+      });
+    });
+  }
+
+  Widget criarItemLista(context, index) {
+    // final item = _listaTarefas[index]["titulo"];
+
+    Map<String, dynamic> _ultimoRemovido = Map();
+    return Dismissible(
+        key: Key(DateTime.now().microsecondsSinceEpoch.toString()),
+        direction: DismissDirection.endToStart,
+        onDismissed: (direction) {
+          //recuperar ultimo item excluido
+
+          _ultimoRemovido = _listaTarefas[index];
+
+          //Remove item da lista
+          _listaTarefas.removeAt(index);
+          _salvarArquivo();
+
+          //snack
+          final snackbar = SnackBar(
+            content: Text("Tarefa Removida"),
+            duration: Duration(seconds: 5),
+            action: SnackBarAction(
+                label: "Desfazer",
+                onPressed: () {
+                  setState(() {
+                    _listaTarefas.insert(index, _ultimoRemovido);
+                  });
+                  _salvarArquivo();
+                }),
+          );
+          Scaffold.of(context).showSnackBar(snackbar);
+        },
+        background: Container(
+          color: Colors.red,
+          padding: EdgeInsets.all(16),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: const [
+              Icon(
+                Icons.delete,
+                color: Colors.white,
+              )
+            ],
+          ),
+        ),
+        child: CheckboxListTile(
+          title: Text(_listaTarefas[index]['titulo']),
+          value: _listaTarefas[index]['realizada'],
+          onChanged: (valorAlterado) {
+            setState(() {
+              _listaTarefas[index]['realizada'] = valorAlterado;
+            });
+
+            _salvarArquivo();
+          },
+        ));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    //_salvarArquivo();
+    print("itens: " + _listaTarefas.toString());
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Lista de tarefas"),
+        backgroundColor: Colors.purple,
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+      floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          backgroundColor: Colors.purple,
+          onPressed: () {
+            showDialog(
+                context: context,
+                builder: (context) {
+                  return AlertDialog(
+                    title: Text("Adicionar Tarefa"),
+                    content: TextField(
+                      controller: _controllerTarefa,
+                      decoration:
+                          const InputDecoration(labelText: "Digite sua tarefa"),
+                      onChanged: (text) {},
+                    ),
+                    actions: [
+                      FlatButton(
+                        child: Text("Cancelar"),
+                        onPressed: () => Navigator.pop(context),
+                      ),
+                      FlatButton(
+                        child: Text("Salvar"),
+                        onPressed: () {
+                          //salvar
+                          _salvarTarefa();
+                          Navigator.pop(context);
+                        },
+                      )
+                    ],
+                  );
+                });
+          }),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: ListView.builder(
+                itemCount: _listaTarefas.length, itemBuilder: criarItemLista),
+          )
+        ],
+      ),
+    );
+  }
+}
